9f28256cda7bb71f38914dffc85ccb1b520f36d7
stammisetty1 <stammisetty@binghamton.edu>
2024-03-20 15:14:58 -0400

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Siva's Portfolio`,
+    siteTitleAlt: `Siva Tammisetty Portfolio`,
+    siteHeadline: `Siva Ashok Kumar Tammisetty Portfolio`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `Portfolio of Siva Ashok Kumar Tammisetty`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@sivatammisetty1`,
   },
   trailingSlash: `always`,
   plugins: [
